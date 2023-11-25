57f7701efcdbed2eeed898ff85df0b0e867e6f4c
ainturNihal <151900700+ainturNihal@users.noreply.github.com>
2023-11-25 22:27:01 +0600

    Update main (2).py

--- main (2).py
+++ main (2).py
@@ -2,4 +2,6 @@ from termcolor import colored
 import time
 import random
 print(colored("                        The last breath", "red", attrs=["bold"]))
+time.sleep(2)
+player_name = input(colored("Enter your name: ", "white"))
 
