4fd92a345367b3f8e06c52cfd6dcea73b834988f
ainturNihal <151900700+ainturNihal@users.noreply.github.com>
2023-11-25 22:24:45 +0600

    Update main (2).py

--- main (2).py
+++ main (2).py
@@ -1,4 +1,5 @@
 from termcolor import colored
 import time
 import random
-print(colored("         The last breath", "red", attrs=["bold","underline"]))
+print(colored("                        The last breath", "red", attrs=["bold"]))
+
