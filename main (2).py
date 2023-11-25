76ffd8ae6dfff80f03ef70e671f1edfb6361edd2
ainturNihal <151900700+ainturNihal@users.noreply.github.com>
2023-11-25 23:07:00 +0600

    Update main (2).py

--- main (2).py
+++ main (2).py
@@ -14,3 +14,16 @@ print(f"The local people knows you as {player_name} Fuji.")
 time.sleep(1)
 print("You livewith your garndmother")
 print(colored("Aya Fujiwa","green"))
+time.sleep(1)
+print(colored("You never met your parents", "red"))
+time.sleep(1)
+print("Just today you have got an offer from a nearby restrurent called Yakiniku Suuchan, and today at 5:30pm you have to go for that interview")
+time.sleep(3)
+print(f"{player_name} Thinking...")
+c = input("It's already 12:45pm should I take shower? (y/n): ")
+if c == "y":
+  print(colored("It's better to take shower before I show up to my interview,"green"))
+else:
+  print("Nah, I am plenty cool these showers don't matter to me I am always the best")
+  print("HAHA!")
+
