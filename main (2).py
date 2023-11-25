7ac79ac36cc20eb8979ac5b0a153c393a9bcad5f
ainturNihal <151900700+ainturNihal@users.noreply.github.com>
2023-11-25 22:51:57 +0600

    Update main (2).py

--- main (2).py
+++ main (2).py
@@ -3,5 +3,14 @@ import time
 import random
 print(colored("                        The last breath", "red", attrs=["bold"]))
 time.sleep(2)
-player_name = input(colored("Enter your name: ", "white"))
-
+player_name = input(colored("\n\n\nEnter your first name: ","white"))
+time.sleep(1)
+print(f"{player_name}, you live in")
+print(colored("Yukicho, Hiroshima in Japan","blue"))
+time.sleep(2)
+print("A bit far away from crowded area")
+time.sleep(1)
+print(f"The local people knows you as {player_name} Fuji.")
+time.sleep(1)
+print("You livewith your garndmother")
+print(colored("Aya Fujiwa","green"))
